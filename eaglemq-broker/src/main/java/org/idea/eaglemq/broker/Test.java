9c5d73683ac201b895992830bd7540786cf8ada2
linhao <hao.lin2@zhenai.com>
2024-03-17 09:41:17 +0800

    init

---
+++ eaglemq-broker/src/main/java/org/idea/eaglemq/broker/Test.java
@@ -0,0 +1,13 @@
+package org.idea.eaglemq.broker;
+
+/**
+ * @Author idea
+ * @Date: Created in 09:40 2024/3/17
+ * @Description
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("this is test");
+    }
+}
